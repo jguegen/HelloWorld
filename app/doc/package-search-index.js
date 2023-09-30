7a5705ac4fe652366a1daab35b7846838275492b
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-09-30 13:38:33 +0200

    Solución correcta donde se aplica el fichero .gitignore

--- app/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.juanlugg8.helloworld"}];updateSearchResults();
--- app/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.juanlugg8.helloworld","l":"MainActivity"}];updateSearchResults();
